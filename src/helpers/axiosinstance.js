90b9bb43ed2517321ca98b61a195c14ced0af722
PR Things <89149952+prthings@users.noreply.github.com>
2024-10-09 10:18:35 +0530

    Update axiosinstance.js

--- src/helpers/axiosinstance.js
+++ src/helpers/axiosinstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL:"http://localhost:4000"
+    baseURL:"http://yo04w8go84ogc8ss888o8wwc.69.197.142.167.sslip.io:4000"
 })
 
 export default axiosInstance;
